ccecbfd1646cb1e1a07e74b967fe60c70a5d4961
elyseniyomugabo <149774229+elyseniyomugabo@users.noreply.github.com>
2023-11-07 14:39:30 +0200

    Add files via upload

---
+++ DowhileloopCDown1.java
@@ -0,0 +1,10 @@
+public class DowhileloopCDown1 {
+    public static void main(String[] args) {
+        int bara = 10;
+
+        do {
+            System.out.println(bara);
+            bara--;
+        } while (bara >= 1);
+    }
+}
---
+++ whileloopCountdown2.java
@@ -0,0 +1,12 @@
+public class whileloopCountdown2 {
+    public static void main(String[] args) {
+        int manuka = 10;
+
+        while (manuka >= 1) {
+            System.out.println(manuka);
+            manuka--;
+        }
+
+        System.out.println("Murakoze!");
+    }
+}
